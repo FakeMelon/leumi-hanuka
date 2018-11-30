a60b57e987466592e9a8f95c013ed0256282679d
FakeMelon <oren.israel5@gmail.com>
2018-11-30 20:00:50 +0200

    First commit

---
+++ public/build/precache-manifest.de881874986f58ce30f709f3f58d9514.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d9038ff940e149a1d28",
+    "url": "/static/js/main.3d9038ff.chunk.js"
+  },
+  {
+    "revision": "a3889070b8c9f9f78c2b",
+    "url": "/static/js/1.a3889070.chunk.js"
+  },
+  {
+    "revision": "3d9038ff940e149a1d28",
+    "url": "/static/css/main.0885a809.chunk.css"
+  },
+  {
+    "revision": "ceb6cb24a6d3681027923465bbcb7869",
+    "url": "/index.html"
+  }
+];

414943338c6b3d9d465c0471bd23491c014ea236
FakeMelon <oren.israel5@gmail.com>
2018-12-01 21:04:59 +0200

    Added candle and title to gallery. Fit gallery to candle.

--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c327050ac111f0323587f3b67a97745.js"
+  "/precache-manifest.74e3e86c15ac7dba0be4fc26152dcf66.js"
 );
 
 workbox.clientsClaim();

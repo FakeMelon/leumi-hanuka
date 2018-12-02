d4db677ae1184446b26247dbeae36181d243ec8f
FakeMelon <oren.israel5@gmail.com>
2018-12-02 21:38:27 +0200

    Integrated firebase to save signing in stats. Made password handling more elegant.

---
+++ public/build/precache-manifest.09eaae367e950a93a0d1aef25c408579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29eb481f95a40bfd7c50",
+    "url": "/static/js/main.29eb481f.chunk.js"
+  },
+  {
+    "revision": "f9f5b2261c449c36b1a7",
+    "url": "/static/js/1.f9f5b226.chunk.js"
+  },
+  {
+    "revision": "29eb481f95a40bfd7c50",
+    "url": "/static/css/main.b7e669c9.chunk.css"
+  },
+  {
+    "revision": "2f12558794b05c142d1d01ca49834daa",
+    "url": "/index.html"
+  }
+];
